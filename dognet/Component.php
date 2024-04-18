a695f8032ef6f560be6a9a97ebd1ab8ab0dfbb4b
gymbeam_tj\Tereza Juzova <tereza.juzova1@gmail.com>
2024-04-18 18:12:19 +0200

    Modify

--- dognet/Component.php
+++ dognet/Component.php
@@ -125,8 +125,8 @@ class Component extends BaseComponent
 
         // Filters
         #$request->addFilter('dateinserted', Gpf_Data_Filter::DATERANGE_IS, constant($dataFilterConstant));
-        $request->addFilter('dateinserted', 'D>=', '2023-06-30');
-        $request->addFilter('dateinserted', 'D<=', '2023-12-31');
+        $request->addFilter('dateinserted', 'D>=', '2023-12-31');
+        $request->addFilter('dateinserted', 'D<=', '2024-02-29');
         
         // Empty array
         $allRecords = [];
