d8cc2ee3acf0e36e6846932fe49d02ccade1acb2
gymbeam_tj\Tereza Juzova <tereza.juzova1@gmail.com>
2025-04-13 16:07:14 +0200

    Modified

--- tests/Logger/StreamTester.php
+++ tests/Logger/StreamTester.php
@@ -33,7 +33,6 @@ class StreamTester extends php_user_filter
     public function filter($in, $out, &$consumed, $closing): int
     {
         while ($bucket = stream_bucket_make_writeable($in)) {
-            /** @var \stdClass $bucket */
             self::$content .= $bucket->data;
             $consumed += $bucket->datalen;
             stream_bucket_append($out, $bucket);
